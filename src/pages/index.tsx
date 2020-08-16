fb977dec4e27a7b57cd89384fe72a6bf897a102e
adampagels <pagels@hotmail.ca>
2020-08-16 16:14:27 -0400

    Remove default Links and footer info

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -37,8 +37,6 @@ const IndexPage = ({ data }: { data: any }) => {
           /* </Link> */
         ))}
       </div>
-      <Link to="/page-2/">Go to page 2</Link> <br />
-      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
     </Layout>
   )
 }
