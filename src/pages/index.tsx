5f219f7db5769aa52deb521308d20e5813576d57
adampagels <pagels@hotmail.ca>
2020-08-16 13:34:40 -0400

    Display gallery of photos from Contentful

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,22 +1,45 @@
 import React from "react"
-import { Link } from "gatsby"
-
+import { Link, graphql } from "gatsby"
+import Img from "gatsby-image"
 import Layout from "../components/layout"
-import Image from "../components/image"
 import SEO from "../components/seo"
 
-const IndexPage = () => (
-  <Layout>
-    <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link> <br />
-    <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
-  </Layout>
-)
+const IndexPage = ({ data }: { data: any }) => {
+  const gallery = data.allContentfulHomeGallery.edges
+
+  return (
+    <Layout>
+      <SEO title="Home" />
+      {gallery.map(({ node: photo }: { node: any }) => (
+        <div>
+          <Link to={`/${photo.slug}`}>
+            <img alt={photo.photo.file.url} src={photo.photo.file.url}></img>
+          </Link>
+        </div>
+      ))}
+      <Link to="/page-2/">Go to page 2</Link> <br />
+      <Link to="/using-typescript/">Go to "Using TypeScript"</Link>
+    </Layout>
+  )
+}
 
 export default IndexPage
+
+export const query = graphql`
+  query IndexPageQuery {
+    allContentfulHomeGallery(sort: { fields: order }) {
+      edges {
+        node {
+          id
+          title
+          slug
+          photo {
+            file {
+              url
+            }
+          }
+        }
+      }
+    }
+  }
+`
