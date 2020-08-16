0b0357c5fa97486f06d88b575003c4a8e02b9c5e
adampagels <pagels@hotmail.ca>
2020-08-15 20:27:43 -0400

    Create pages for individual photos and display via photoTemplate

--- gatsby-node.js
+++ gatsby-node.js
@@ -1,7 +1,41 @@
-/**
- * Implement Gatsby's Node APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/node-apis/
- */
+const path = require(`path`)
+const slash = require(`slash`)
+exports.createPages = ({ graphql, actions }) => {
+  const { createPage } = actions
 
-// You can delete this file if you're not using it
+  return graphql(
+    `
+      {
+        allContentfulHomeGallery {
+          edges {
+            node {
+              id
+              slug
+            }
+          }
+        }
+      }
+    `
+  )
+    .then(result => {
+      if (result.errors) {
+        console.log("Error retrieving contentful data", result.errors)
+      }
+
+      const photoTemplate = path.resolve("./src/templates/photo.js")
+
+      result.data.allContentfulHomeGallery.edges.forEach(edge => {
+        createPage({
+          path: `/${edge.node.slug}/`,
+          component: slash(photoTemplate),
+          context: {
+            slug: edge.node.slug,
+            id: edge.node.id,
+          },
+        })
+      })
+    })
+    .catch(error => {
+      console.log("Error retrieving contentful data", error)
+    })
+}
